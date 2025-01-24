0c6edd4a548513ec15418e92939b30b7ceaf9669
JacobW81 <jacobworthington@students.abtech.edu>
2025-01-24 11:51:06 -0500

    Update 2.2 Simple Messages.py

--- 2.2 Simple Messages.py
+++ 2.2 Simple Messages.py
@@ -1,9 +1,4 @@
-Python 3.13.1 (tags/v3.13.1:0671451, Dec  3 2024, 19:06:28) [MSC v.1942 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license" for more information.
-
 >>> message = "Hello, Jacob! Welcome to Python programming."
 >>> print(message)
-Hello, Jacob! Welcome to Python programming.
 >>> message = "Hurray for Python"
 >>> print(message)
-Hurray for Python
